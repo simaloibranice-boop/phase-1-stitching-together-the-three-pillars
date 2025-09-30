92a03cc071314364aeb5858f98b07f58d5051f88
Branice Simaloi <simaloibranice@gmail.com>
2025-10-01 01:19:52 +0300

    Completed Simple Liker functionality

--- demo.js
+++ demo.js
@@ -1,62 +1,45 @@
-// We use the Objects below to control toggling like / unlike status
+// Step 1: DOM Manipulation
+// Grab all the heart elements on the page
+const articleHearts = document.querySelectorAll(".like-glyph");
 
-const glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
-};
-
-const colorStates = {
-  "red" : "",
-  "": "red"
-};
-
-// STEP 1: The line of code below is what lets JavaScript find the elements that
-// we want to make clickable. Without JavaScript, clicking on these heart shapes
-// does nothing. Uncomment the code and refresh the demo page. 
-
-// const articleHearts = document.querySelectorAll(".like-glyph");
-
-function likeCallback(e) {
-  const heart = e.target;
-  mimicServerCall()
-    .then(function(serverMessage){
-      // STEP 2: Uncomment the 3 lines after the alert.
-      // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
-      // mimicking Pillar 3 (Server Communication) to only update the screen if
-      // the sending of information to the server succeeds.
-      alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      alert("Something went wrong!");
-    });
-}
-
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, we need to add a click event listener to the elements we identified in
-// STEP 1. That's Pillar 2, event handling. Uncomment this code:
-
-// for (const glyph of articleHearts) {
-//   glyph.addEventListener("click", likeCallback);
-// }
-
-// STEP 4: 
-
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
-function mimicServerCall() {
+// Step 2: Mock Server Communication
+// This function simulates talking to a server
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      resolve("Pretend remote server notified of action!");
+      // randomly fail 20% of the time
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
     }, 300);
   });
 }
+
+// Step 3: Event Listening
+// Add a click listener to each heart
+articleHearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    // Call the mock server
+    mimicServerCall()
+      .then(() => {
+        // Toggle heart state
+        if (heart.innerText === "♡") {
+          heart.innerText = "♥";
+          heart.classList.add("activated-heart");
+        } else {
+          heart.innerText = "♡";
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch((error) => {
+        // Show server error message
+        const errorModal = document.getElementById("modal");
+        errorModal.classList.remove("hidden");
+        errorModal.innerText = error;
+        setTimeout(() => errorModal.classList.add("hidden"), 3000);
+      });
+  });
+});
